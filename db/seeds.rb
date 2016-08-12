732675b947c36719d377ed8c84686dcd14614906
Shusuke Kakiuchi <kakiuchis@gmail.com>
2016-08-12 09:58:27 +0000

    dive14-ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+10.times do |n|
+    picture = Picture.new
+    image_src = File.join(Rails.root, "/db/fixtures/#{n+1}.jpg")
+    src_file = File.new(image_src)
+    picture.img = src_file
+    picture.content = "comment#{n+1}"
+    picture.user_id = 1
+    picture.save
+end
+
+
